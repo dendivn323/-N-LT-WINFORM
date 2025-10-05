75f9585be8f5e15fd990d712970eb986e5a7520f
Le Hieu <dendivn322@gmail.com>
2025-10-05 16:17:21 +0700

    HIẾU CODE

--- QLNhanVien/Froms/FrmLogin.cs
+++ QLNhanVien/Froms/FrmLogin.cs
@@ -70,7 +70,8 @@ namespace QLNhanVien
         {
             
             //SỬA CODE CỦA HIẾU
-            //Lấy ra user pass 
+            //Lấy ra user pass
+            //HIẾU CODE
             txtUserName.Text = Properties.Settings.Default.UserName;
             txtPassword.Text = Properties.Settings.Default.PassWord;
             ckcSavePass.Checked = Properties.Settings.Default.SavePass;
